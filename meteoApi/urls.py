2b192f0603172748dd0382f93287102c2e9c7dcc
Vladimir Yazadzhiev <73740415+VladdyOnGitHub@users.noreply.github.com>
2024-02-22 12:26:39 +0100

    [#3] REFACTOR: Update urls to match the project name (#4)
    ## Author section
    
    Closes #3 
    
    ### Statement of change
    
    <!-- WRITE A CONCISE STATEMENT OF CHANGE -->
    
    - Changed urls from `meteoApi/` to `simple-weather/`
    
    ### Prerequisites
    
    - None
    <!-- SPECIFY ANY REQUIREMENTS AND/OR SOURCES (BUILDS) -->
    
    ### Specific Test Objectives (STO)
    
    <!-- TEST THIS AND EXPECT THAT -->
    
    - [ ] The app works, and the urls are changed.
    
    
    ## Reviewer section
    
    ### Requirements
    Any and all failures caused by the changes provided by this MR shall be
    reported as comments to this MR.
    
    ### Checklist
    The _Reviewer_ certifies that:
    - [ ] The scope of the parent Issue is met.
    - [ ] Code changes meet the requirements and is found to be of high
    quality.
    - [ ] The functional and visual changes are validated locally and no
    failures or cause for concern was discovered.

--- meteoApi/urls.py
+++ meteoApi/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from meteoApi import views
 
 urlpatterns = [
-    path("meteoApi/", views.local_temp, name="local_temp"),
-    path("meteoApi/discover", views.random_location_temp, name="random_location_temp")
+    path("simple-weather/", views.local_temp, name="local_temp"),
+    path("simple-weather/discover", views.random_location_temp, name="random_location_temp")
 ]
